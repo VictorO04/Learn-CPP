80baf46bd5262290acb66cbff799ed7f72903cbc
Victor <Victorfoliveira0407@gmail.com>
2025-09-11 20:29:24 -0300

    Inversão de variáveis

---
+++ conteudo/variaveis/inversaoVariaveis.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Inverter variável: -variavel
+
+    int num1 = 10;
+
+    cout << num1 << "\n";
+
+    num1 = -num1;
+
+    cout << num1 << "\n";
+
+    return 0;
+}
